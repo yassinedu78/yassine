86f45ead7a8ac49026be67006308a17f3d6c1a6d
Yassinedu78 <diziyassine@gmail.com>
2021-11-15 12:24:29 +0100

    initialisation du projet + première version

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@300&display=swap" rel="stylesheet">
+
+    <title>Yassine</title>
+
+    <style>
+        body{
+            font-family: 'Zilla Slab', serif;
+        }
+
+        h1 {
+            font-family: 'Amatic SC', cursive;
+        }
+    </style>
+  </head>
+  <body>
+    <h1 class="text-center">Yassine</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam vero iure eum, consequatur minima, eligendi non autem voluptatum ratione tempore labore praesentium architecto fugit eius veniam porro quidem eos repellendus officiis. Distinctio laboriosam hic qui, maiores eaque deserunt accusamus sequi? Mollitia reiciendis aliquid qui porro corrupti, illum minima sint voluptatum, eaque nobis minus voluptates quod eveniet officiis deleniti repudiandae aspernatur labore non quidem odio optio nam at? Nemo, esse! Possimus enim optio, doloribus beatae vitae odit maxime debitis praesentium blanditiis id deserunt minima qui corporis magnam exercitationem incidunt molestiae harum sit esse non eos. Nulla, quisquam, ullam autem voluptate ipsam repellat nihil doloribus, sapiente magnam distinctio libero ea! Aut corporis laboriosam soluta. Fugiat, autem illum! Aperiam quibusdam qui impedit molestiae natus illum ipsum neque ut iusto cupiditate et consequatur ab possimus dolor est corporis quam, ducimus error. Voluptatem fugit maxime ad fuga illo tempore temporibus, doloremque laboriosam repellat, consequuntur itaque ratione repellendus perspiciatis? Nulla similique vitae quas repellat, vero ratione optio adipisci neque quisquam in enim quibusdam eius saepe deserunt quos non. Error delectus placeat, fuga, qui minus atque, accusamus reprehenderit suscipit excepturi neque nesciunt quas et voluptates! Eum cumque tenetur totam. Rem libero hic sunt repellendus recusandae eum a iure harum tempora? Voluptas nemo animi, maiores laboriosam fuga sapiente rerum delectus facilis reiciendis recusandae ea accusantium, reprehenderit eum totam rem magnam veritatis impedit explicabo amet qui quia magni cumque. Natus ut neque deleniti consequuntur modi provident, odit omnis officiis autem, tenetur libero, aliquam cum tempora temporibus at. Cum.</p>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
