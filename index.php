9eab2cefd7f1b694afa951a55221fcf838db7b18
Yassinedu78 <diziyassine@gmail.com>
2021-11-15 14:15:43 +0100

    dev layout page index + création fichier style

--- index.php
+++ index.php
@@ -31,8 +31,38 @@
   <body>
     <h1 class="text-center">Yassine</h1>
 
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="images/audi-rs3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="images/audi-rs6.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="images/GLE-AMG.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam vero iure eum, consequatur minima, eligendi non autem voluptatum ratione tempore labore praesentium architecto fugit eius veniam porro quidem eos repellendus officiis. Distinctio laboriosam hic qui, maiores eaque deserunt accusamus sequi? Mollitia reiciendis aliquid qui porro corrupti, illum minima sint voluptatum, eaque nobis minus voluptates quod eveniet officiis deleniti repudiandae aspernatur labore non quidem odio optio nam at? Nemo, esse! Possimus enim optio, doloribus beatae vitae odit maxime debitis praesentium blanditiis id deserunt minima qui corporis magnam exercitationem incidunt molestiae harum sit esse non eos. Nulla, quisquam, ullam autem voluptate ipsam repellat nihil doloribus, sapiente magnam distinctio libero ea! Aut corporis laboriosam soluta. Fugiat, autem illum! Aperiam quibusdam qui impedit molestiae natus illum ipsum neque ut iusto cupiditate et consequatur ab possimus dolor est corporis quam, ducimus error. Voluptatem fugit maxime ad fuga illo tempore temporibus, doloremque laboriosam repellat, consequuntur itaque ratione repellendus perspiciatis? Nulla similique vitae quas repellat, vero ratione optio adipisci neque quisquam in enim quibusdam eius saepe deserunt quos non. Error delectus placeat, fuga, qui minus atque, accusamus reprehenderit suscipit excepturi neque nesciunt quas et voluptates! Eum cumque tenetur totam. Rem libero hic sunt repellendus recusandae eum a iure harum tempora? Voluptas nemo animi, maiores laboriosam fuga sapiente rerum delectus facilis reiciendis recusandae ea accusantium, reprehenderit eum totam rem magnam veritatis impedit explicabo amet qui quia magni cumque. Natus ut neque deleniti consequuntur modi provident, odit omnis officiis autem, tenetur libero, aliquam cum tempora temporibus at. Cum.</p>
 
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore sit accusamus consequatur distinctio placeat aliquid repudiandae, nobis exercitationem, nesciunt nemo obcaecati, velit officiis quod voluptas. Eveniet odit quasi eligendi sed necessitatibus voluptatem pariatur ipsa odio rem non, beatae quis vitae libero nulla facilis reprehenderit placeat maiores repellat? Consequatur ipsum dolores fugiat tenetur cumque id. Exercitationem dignissimos suscipit hic odio aperiam molestias vitae, voluptatibus officia laborum asperiores soluta magnam corporis officiis sunt dolorum nam nemo totam distinctio illum? Laudantium molestias ea sed nobis quisquam sunt necessitatibus quidem consequuntur voluptatum dolor eaque natus repudiandae, nulla voluptas neque distinctio quasi facilis ducimus sapiente?</p>
+
+  
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
